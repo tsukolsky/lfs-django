d1fa04fd4a16a9f41cc7ed0b976e6cf44fa2d57f
Todd Sukolsky <tsukolsky@gmail.com>
2025-04-11 12:50:45 -0400

    comments explaining how to call a command from a command

--- lastfanstanding/management/commands/close_week.py
+++ lastfanstanding/management/commands/close_week.py
@@ -1,12 +1,24 @@
 from django.core.management.base import BaseCommand
+from django.core.management import call_command
 from django.utils import timezone
 from lastfanstanding.models import LFSGame, LFSPick
 
 class Command(BaseCommand):
-    help = "Closes current week of LFS"
+    help = "Closes current week of LFS if the criteria is met"
 
     def handle(self, *args, **options):
-        # do something
+        """
+        # First, get the current LFS game and see what week it is
+        # Based on the week, this needs to do a few things
+        # 1) Close out teams that are now closed out, if they were not already. This script should be called frequently,
+             so it cannot just update the week/clouseouts every time it is called. It needs to assess the current date
+             and make sure it's still with in the week. once sunday 1300 hits, the next week can roll over.
+        """
+
+        """
+        You can call a command from python using the call_command(<command string>)
+        For example, to call this command would be call_command('close_week')
+        """
         print(f"Closing week: Increment week number, check which teams won and lost on the picks")
         current_game = LFSGame.objects.get(year=2025)
         newweek = current_game.current_week + 1
